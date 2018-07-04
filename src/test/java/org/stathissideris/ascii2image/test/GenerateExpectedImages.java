0cdb2bfb97da1732f97179bc5f29c680710fff7c
Nathan Jackson <nathan@csnate.com>
2018-07-03 22:31:23 -0400

    Setup gradle build system.

--- src/test/java/org/stathissideris/ascii2image/test/GenerateExpectedImages.java
+++ src/test/java/org/stathissideris/ascii2image/test/GenerateExpectedImages.java
@@ -28,7 +28,7 @@ package org.stathissideris.ascii2image.test;
  */
 public class GenerateExpectedImages {
 	public static void main(String[] args) {
-		VisualTester.generateImages(VisualTester.getFilesToRender(), "tests/images-expected");
+		VisualTester.generateImages(VisualTester.getFilesToRender(), "test-resources/images-expected");
 		System.out.println("Done");	
 	}
 }
